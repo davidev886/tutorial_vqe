54c26ebc24f5598bb4538461aa780dd2caee14c2
Davide Vodola <davide.vodola@basf.com>
2024-08-28 09:47:04 +0200

    Removed class; merged function in vqe

--- main_vqe.py
+++ main_vqe.py
@@ -2,14 +2,13 @@
 Contains the main file for running a complete VQE with cudaquantum
 """
 
-from src.utils_pyscf import System
+from src.vqe_cudaq_qnp import get_molecular_hamiltonian
 
 geometry = "systems/geo_o3.xyz"
 
 num_active_orbitals = 6
 num_active_electrons = 8
 
-system_o3 = System(geometry=geometry,
-                   num_active_electrons=num_active_electrons,
-                   num_active_orbitals=num_active_orbitals)
-
+hamiltonian_o3 = get_molecular_hamiltonian(geometry=geometry,
+                                           num_active_electrons=num_active_electrons,
+                                           num_active_orbitals=num_active_orbitals)
