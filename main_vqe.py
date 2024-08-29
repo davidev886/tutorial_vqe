751782e27438804b2ca6963bd1219a7b3397ee4b
Davide Vodola <davide.vodola@basf.com>
2024-08-29 17:06:59 +0200

    Adapted for fenta

--- main_vqe.py
+++ main_vqe.py
@@ -1,18 +1,26 @@
 """
 Contains the main file for running a complete VQE with cudaquantum
 """
-
+import numpy as np
 from src.vqe_cudaq_qnp import get_molecular_hamiltonian
 from src.vqe_cudaq_qnp import VQE
+from src.vqe_cudaq_qnp import get_cudaq_hamiltonian
+import pickle
 
+jw_hamiltonian_file = ""
 geometry = "systems/geo_o3.xyz"
 
-num_active_orbitals = 6
-num_active_electrons = 8
-spin = 0
-hamiltonian_o3, constant_term = get_molecular_hamiltonian(geometry=geometry,
-                                                          num_active_electrons=num_active_electrons,
-                                                          num_active_orbitals=num_active_orbitals)
+num_active_orbitals = 12
+num_active_electrons = 9
+spin = 1
+if jw_hamiltonian_file:
+    with open(r"jw_hamiltonian_file", "rb") as hamiltonian_file:
+        jw_hamiltonian = pickle.load(hamiltonian_file)
+    hamiltonian, constant_term = get_cudaq_hamiltonian(jw_hamiltonian)
+else:
+    hamiltonian, constant_term = get_molecular_hamiltonian(geometry=geometry,
+                                                           num_active_electrons=num_active_electrons,
+                                                           num_active_orbitals=num_active_orbitals)
 
 MINIMIZE_METHODS_NEW_CB = ['nelder-mead', 'powell', 'cg', 'bfgs', 'newton-cg',
                            'l-bfgs-b', 'trust-constr', 'dogleg', 'trust-ncg',
@@ -20,9 +28,9 @@ MINIMIZE_METHODS_NEW_CB = ['nelder-mead', 'powell', 'cg', 'bfgs', 'newton-cg',
 
 # Define some options for the VQE
 options = {'n_vqe_layers': 1,
-           'maxiter': 100,
+           'maxiter': 1000,
            'energy_core': constant_term,
-           'return_final_state_vec': True,
+           'return_final_state_vec': False,
            'optimizer': 'nelder-mead'}
 
 n_qubits = 2 * num_active_orbitals
@@ -32,7 +40,11 @@ vqe = VQE(n_qubits=n_qubits,
           spin=spin,
           options=options)
 
-results = vqe.execute(hamiltonian_o3)
+results = vqe.execute(hamiltonian)
 
 # Best energy from VQE
 optimized_energy = results['energy_optimized']
+
+energy_optimized = results['callback_energies']
+
+np.savetxt("energies.dat", energy_optimized)
