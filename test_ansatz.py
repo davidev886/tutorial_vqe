4a7ddabc12197b077d90953015a04f7159f93b6f
Davide Vodola <davide.vodola@basf.com>
2024-09-18 13:20:40 +0200

    typo

--- test_ansatz.py
+++ test_ansatz.py
@@ -14,7 +14,7 @@ from typing import List
 @cudaq.kernel
 def vqe_ansatz(params: List[float], input_state: List[complex]):
     thetas = params
-    n_qubits = int(np.log2(input_state.size))
+    n_qubits = int(np.log2(len(input_state)))
     n_layers = 1
     number_of_blocks = n_qubits // 2 - 1
 
