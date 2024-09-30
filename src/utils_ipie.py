90bc5ea2e244866c8d5bc6d30d66d411c6dda962
Davide Vodola <davide.vodola@basf.com>
2024-09-30 23:16:42 +0200

    Simplified for mcscf trial

--- src/utils_ipie.py
+++ src/utils_ipie.py
@@ -66,10 +66,8 @@ def gen_ipie_input_from_pyscf_chk(
         verbose: bool = True,
         chol_cut: float = 1e-5,
         ortho_ao: bool = False,
-        mcscf: bool = False,
-        linear_dep_thresh: float = 1e-8,
         num_frozen_core: int = 0):
-    """Generate AFQMC data from PYSCF (molecular) simulation.
+    """Generate AFQMC data from PYSCF (molecular) MCSCF simulation.
         Adapted from ipie.utils.from_pyscf: returns hamiltonian and wavefunction instead of writing on files
     """
     # if mcscf:
@@ -105,26 +103,8 @@ def gen_ipie_input_from_pyscf_chk(
     )
     # write_hamiltonian(ham.H1[0], copy_LPX_to_LXmn(ham.chol), ham.ecore, filename=hamil_file)
     ipie_ham = (ham.H1[0], copy_LPX_to_LXmn(ham.chol), ham.ecore)
-    nelec = (mol.nelec[0] - num_frozen_core, mol.nelec[1] - num_frozen_core)
-    if verbose:
-        print(f"# Number of electrons in simulation: {nelec}")
-    if mcscf:
-        # ci_coeffs = scf_data["ci_coeffs"]
-        # occa = scf_data["occa"]
-        # occb = scf_data["occb"]
-        # write_wavefunction((ci_coeffs, occa, occb), wfn_file)
-        return ipie_ham
-    else:
-        wfn = generate_wavefunction_from_mo_coeff(
-            mo_coeffs,
-            mo_occ,
-            basis_change_matrix,
-            nelec,
-            ortho_ao=ortho_ao,
-            num_frozen_core=num_frozen_core,
-        )
-        # write_wavefunction(wfn, wfn_file)
-        return ipie_ham, wfn
+
+    return ipie_ham
 
 
 def get_afqmc_data(scf_data, final_state_vector, chol_cut=1e-5):
@@ -148,7 +128,6 @@ def get_afqmc_data(scf_data, final_state_vector, chol_cut=1e-5):
     """
 
     h1e, cholesky_vectors, e0 = gen_ipie_input_from_pyscf_chk(scf_data,
-                                                              mcscf=True,
                                                               chol_cut=chol_cut)
     molecule = scf_data["mol"]
     n_active_electrons = scf_data["num_active_electrons"]
