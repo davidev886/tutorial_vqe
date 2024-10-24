a7dbc7de5ec1ef6c932dbe61a796073a972619d0
Davide Vodola <davide.vodola@basf.com>
2024-10-24 15:14:53 +0200

    Add count terms

--- src/utils_ipie.py
+++ src/utils_ipie.py
@@ -259,6 +259,7 @@ def get_molecular_hamiltonian(
         end = time.time()
         if verbose:
             print("# Time for preparing the cudaq Hamiltonian:", end - start)
+            print('Total number of terms in the spin hamiltonian = ', hamiltonian_cudaq.get_term_count())
 
         scf_data["energy_core_cudaq_ham"] = energy_core_cudaq_ham
 
